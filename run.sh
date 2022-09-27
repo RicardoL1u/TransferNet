de4786003a6f7b963b6fad299a7e19d80a8527c0
RicardoL1u <2018212874@bupt.edu.cn>
2022-09-27 10:07:00 +0800

    ref: untrack the run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=7
-TRANSFORMERS_OFFLINE=1 python -m WebQSP.train \
-    --input_dir data/AnonyQA \
-    --save_dir data/AnonyQA/ckpt \
-    --kg_name essentail \
-    --batch_size 16 
